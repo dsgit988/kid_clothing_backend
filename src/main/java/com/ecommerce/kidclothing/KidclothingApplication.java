1a7d54bfd020277e7bed6140e7d0ebf57ee7300e
Tom <pjoh1092@gmail.com>
2022-12-09 10:43:06 -0600

    initial commit

---
+++ src/main/java/com/ecommerce/kidclothing/KidclothingApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.kidclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidclothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidclothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/kidclothing/KidclothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.kidclothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidclothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
